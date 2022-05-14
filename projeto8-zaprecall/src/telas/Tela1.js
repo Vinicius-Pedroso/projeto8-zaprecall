3c77ecc2e73f20d95d152016108ba4f28660d50e
Vinicius Pedroso <viniciuspedrosoreis@gmail.com>
2022-05-14 20:15:23 -0300

    layout 1 com import

---
+++ projeto8-zaprecall/src/telas/Tela1.js
@@ -0,0 +1,11 @@
+export default function Tela1() {
+    return (
+        <div class="telabase">
+            <div>
+                <img src="image/logo.png"/>
+                <h2>ZapRecall</h2>
+                <h1>Iniciar Recall</h1>
+            </div>
+        </div>
+    );
+}
