1bd285c97e7316ec237679599e12324b0140e32e
Vinicius Pedroso <viniciuspedrosoreis@gmail.com>
2022-05-15 10:52:10 -0300

    layout base das perguntas

---
+++ projeto8-zaprecall/src/telas/Tela2.js
@@ -0,0 +1,19 @@
+export default function Tela2() {
+    return (
+        <div class="baserecall">
+        <div class="logorecall">
+          <img src="image/logo-pequeno.png"/>
+          <h2>ZapRecall</h2>
+        </div>
+        <div class="perguntas">
+          <div class="pergunta">
+            <h3>Pergunta X</h3>
+            <ion-icon name="play-outline"></ion-icon>
+          </div>
+        </div>
+        <div class="conclusao">
+          <h4>Concluido x/y</h4>
+        </div>
+      </div>
+    );
+}
